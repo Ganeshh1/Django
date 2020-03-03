75a8264755151032e8fe7ad3f3a4ef7f93533585
Ganeshh1 <ganesh.veeramani@aspiresys.com>
2020-03-03 14:31:14 +0530

    Update

--- test_project/blog/models.py
+++ test_project/blog/models.py
@@ -24,7 +24,7 @@ class Post1(models.Model):
     
 class Post2(models.Model):
     name=models.CharField(max_length=100)
-    age=models.URLField()
+    URL=models.URLField()
 
     def __str__(self):
         return 'URLField_Post'
@@ -94,7 +94,7 @@ class Post10(models.Model):
 
 class Post11(models.Model):
     name=models.CharField(max_length=100)
-    age=models.DateField()
+    date=models.DateField()
 
     def __str__(self):
         return 'IntegerField_Post'
@@ -115,7 +115,7 @@ class Post13(models.Model):
 
 class Post14(models.Model):
     name=models.CharField(max_length=100)
-    age=models.DateTimeField()
+    date=models.DateTimeField()
 
     def __str__(self):
         return 'DateTimeField_Post'
